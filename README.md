90b234a833357cc2e2853bed0f51ba4048b97bac
sukesan1984 <takamikousuke@gmail.com>
2016-12-21 00:57:46 +0900

    add api access

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+npm install -g babel-cli 
+npm install rx
+npm install --save-dev babel-preset-es2015
+```
--- index.js
+++ index.js
@@ -3,15 +3,19 @@ import Rx from 'rx'
 // http://stackoverflow.com/questions/21667824/trying-to-make-my-own-rxjs-observable
 var getAccessToken = (function (){
     return function (callback) {
+        console.log("start getAccessToken");
         setTimeout(function(){
-            callback(undefined, "some access token");
+            console.log("end getAccessToken");
+            callback(undefined, "valid");
         }, 5);
     };
 });
 
-var getSomeApiAccessWithAccessToken = (function (accessToken) {
-    return function(callback(){
+var getSomeApiAccessWithAccessToken = (function (accessToken, apiName) {
+    return function(callback){
+        console.log("start " + apiName);
         setTimeout(function(){
+            console.log("end " + apiName);
             if(accessToken === "valid"){
                 callback(undefined, "success");
             } else {
@@ -21,10 +25,8 @@ var getSomeApiAccessWithAccessToken = (function (accessToken) {
     };
 });
 
-
 var getAccessTokenObservable = Rx.Observable.create(function (observable){
     (getAccessToken())(function(err, val){ 
-        console.log("Hoge");
         if(err){
             observable.onError(err);
         } else {
@@ -34,12 +36,40 @@ var getAccessTokenObservable = Rx.Observable.create(function (observable){
     });
 });
 
-getAccessTokenObservable
-    .share()
+var getSomeApiAccessWithAccessTokenObservable = (accessToken, apiName) => {
+    return Rx.Observable.create(function (observable){
+        (getSomeApiAccessWithAccessToken(accessToken, apiName))(function(err, val){
+            if(err){
+                observable.onError(err);
+            } else {
+                observable.onNext(val);
+                observable.onCompleted();
+            }
+        });
+    });
+};
+
+var observable = getAccessTokenObservable
+    .share();
+
+observable
+    .flatMap((accessToken) => {
+        return getSomeApiAccessWithAccessTokenObservable(accessToken, "A");
+    })
     .subscribe(
-        (accessToken) => { console.log('next', accessToken); },
+        (result) => { console.log('next1', result); },
         (err) => { console.log('error', err); },
         () => { console.log('done'); }
     );
 
-(getAccessToken())( () => {});
+observable
+    .flatMap((accessToken) => {
+        return getSomeApiAccessWithAccessTokenObservable(accessToken, "B");
+    })
+    .subscribe(
+        (result) => { console.log('next2', result); },
+        (err) => { console.log('error', err); },
+        () => { console.log('done'); }
+    );
+
+
